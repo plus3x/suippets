c84f364237225fee33c8acdb97beefce844ce726
Carlos costa (c4co) <carllos.nc@gmail.com>
2016-05-11 14:10:32 -0300

    Fixing scroll in modal

--- app/js/components/modal.js
+++ app/js/components/modal.js
@@ -3,6 +3,7 @@ import $ from "jquery";
 function modal() {
   var body = $( "body" );
   var $window = $( window );
+  var $document = $( document  );
   var modalTargets = $( "[modal-target]" ).not( "pre [modal-target]" );
   var modalWrappers = $( "[modal-wrapper]" ).not( "pre [modal-wrapper]" );
   var modalContent = $( "[modal-content]" ).not( "pre [modal-content]" );
@@ -10,35 +11,39 @@ function modal() {
   modalWrappers.detach().addClass( "modal-hidden" ).appendTo( "html" );
 
   function openModal( modalWrapper ) {
-    modalWrapper.removeClass("modal-hidden").addClass("modal-actived");
-    body.css({ "overflow-y": "hidden" });
+    modalWrapper.removeClass( "modal-hidden" ).addClass( "modal-actived" );
   }
 
   function hideModal( modalWrapper ) {
-    modalWrapper.addClass("modal-hidden").removeClass("modal-actived");
-    body.css({ "overflow-y": "scroll" });
+    modalWrapper.addClass( "modal-hidden" ).removeClass( "modal-actived" );
+    $window.off( "scroll" );
   }
 
   modalTargets.each(function( index, element ) {
     var target = $( element );
-    var targetIndex = $( element ).attr("modal-target");
+    var targetIndex = $( element ).attr( "modal-target" );
     var modalWrapper = $( "[modal-wrapper=" + targetIndex + "]" );
-    var modalClose = modalWrapper.find("[modal-close]");
+    var modalClose = modalWrapper.find( "[modal-close]" );
     var modalContent = modalWrapper.find( "[modal-content]" );
 
-    target.on("click", function() {
+    target.on( "click", function() {
       openModal( modalWrapper );
+      var scrollPosition = $document.scrollTop();
+
+      $window.on( "scroll", function() {
+        $( this ).scrollTop( scrollPosition );
+      });
     });
 
     modalContent.on( "click", function( event ) {
       event.stopPropagation();
     });
 
-    modalWrapper.on("click", function() {
+    modalWrapper.on( "click", function() {
       hideModal( modalWrapper );
     });
 
-    modalClose.on("click", function() {
+    modalClose.on( "click", function() {
       hideModal( modalWrapper );
     });
 
