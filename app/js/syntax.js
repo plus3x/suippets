25864c01123240ed83a4662c1655025e167cef47
Carlos Costa <carllos.nc@gmail.com>
2016-07-11 01:50:51 -0300

    Adding sintax

---
+++ app/js/syntax.js
@@ -0,0 +1,25 @@
+import $ from "jquery-slim";
+import hljs from "highlight.js";
+import superagent from "superagent";
+
+function syntax(){
+ let codeWrapper = $('[code]');
+
+  hljs.configure({
+    'languages': ['js', 'html', 'css', 'scss', 'xml'],
+    'useBR': 'true'
+  });
+
+  hljs.initHighlightingOnLoad();
+
+  codeWrapper.each((index, obj) => {
+    let element = $(obj); 
+    let path = element.attr( 'code' );
+ 
+    superagent.get(path).end((err, res) => {
+      element.html( hljs.highlightAuto( res.text ).value );
+    });
+  });
+}
+
+export default syntax; 
