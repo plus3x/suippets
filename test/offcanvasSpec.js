f41ddef40946578e4eb53142e308e95101d8d0f7
c4co <carllos.nc@gmail.com>
2016-08-07 07:43:19 +0100

    add offcanvas test

---
+++ test/offcanvasSpec.js
@@ -0,0 +1,48 @@
+import $ from "jquery-slim";
+import offcanvas from "../app/js/components/offcanvas.js";
+
+describe( "Offcanvas Spec", () => {
+
+  const offcanvasTargetMarkupt = $(`
+    <button offcanvas-target="1"> </button>
+  `);
+
+  const offcanvasWrapperMarkup = $(`
+    <div offcanvas-wrapper="1" class="offcanvas">
+      <div offcanvas-content class="offcanvas-content">
+      </div>
+    </div>
+  `);
+
+  offcanvas.targets = offcanvasTargetMarkupt;
+  offcanvas.wrappers = offcanvasWrapperMarkup;
+
+  it( "initial state, add {offcanvas-hidden} class in wrapper", () => {
+    expect( offcanvas.wrappers ).not.toHaveClass( "offcanvas-hidden" );
+
+    offcanvas.init();
+
+    expect( offcanvas.wrappers).toHaveClass( "offcanvas-hidden" );
+  });
+
+  it( "Show offcanvas, add class {offcanvas-activated} and remove {offcanvas-hidden}", () => {
+    expect( offcanvas.wrappers ).not.toHaveClass( "offcanvas-activated" );
+    expect( offcanvas.wrappers ).toHaveClass( "offcanvas-hidden" );
+
+    offcanvas.show( offcanvas.wrappers );
+
+    expect( offcanvas.wrappers).not.toHaveClass( "offcanvas-hidden" );
+    expect( offcanvas.wrappers).toHaveClass( "offcanvas-activated" );
+  })
+
+  it( "Hide offcanvas, remove class {offcanvas-activated} and add {offcanvas-hidden}", () => {
+    expect( offcanvas.wrappers ).not.toHaveClass( "offcanvas-hidden" );
+    expect( offcanvas.wrappers ).toHaveClass( "offcanvas-activated" );
+
+    offcanvas.hide( offcanvas.wrappers );
+
+    expect( offcanvas.wrappers ).not.toHaveClass( "offcanvas-activated" );
+    expect( offcanvas.wrappers ).toHaveClass( "offcanvas-hidden" );
+  });
+
+});
