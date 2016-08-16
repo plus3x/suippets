f8c5d035f9b7c13ad6e3d65ca14bcd655e74d7af
c4co <carllos.nc@gmail.com>
2016-08-16 21:07:43 +0100

    Add expander spec

---
+++ test/expanderSpec.js
@@ -0,0 +1,33 @@
+import $ from "jquery-slim";
+import expander from "../app/js/components/expander.js";
+
+describe( "expander spec", () => {
+
+  beforeEach( () => {
+    const expandersMarkup = $(`
+      <div expander>
+        <p expander-title > </p>
+        <div expander-content > </div>
+      </div>
+    `);
+
+    expander.expanders = expandersMarkup;
+
+    expander.init();
+  });
+
+  it( "this is my first test", () => {
+    let expanderTitle = expander.expanders.find( "[expander-title]" );
+
+    expect( expanderTitle ).not.toHaveClass( "expander-title-activated" );
+
+    expanderTitle.click();
+
+    expect( expanderTitle ).toHaveClass( "expander-title-activated" );
+
+    expanderTitle.click();
+
+    expect( expanderTitle ).not.toHaveClass( "expander-title-activated" );
+  });
+
+});
