55d663433c7c67d0fd8106fdfa46427637c47f1d
c4co <carllos.nc@gmail.com>
2016-08-21 01:07:37 +0100

    Add new test case

--- test/dropdownSpec.js
+++ test/dropdownSpec.js
@@ -9,12 +9,16 @@ describe( "Dropdown Spec", () => {
         <div dropdown class="dropdown">
           <button dropdown-target class="btn" > </button>
           <div dropdown-content class="dropdown-content"> </div>
+        </div><div dropdown class="dropdown">
+          <button dropdown-target class="btn" > </button>
+          <div dropdown-content class="dropdown-content"> </div>
         </div>
       `
     );
 
     dropdown.all = dropdownMarkup;
     dropdown.contents = dropdownMarkup.find( "[dropdown-content]" );
+    dropdown.targets = dropdownMarkup.find( "[dropdown-target]" );
 
     dropdown.init();
   } );
@@ -32,8 +36,20 @@ describe( "Dropdown Spec", () => {
 
     expect( dropdown.contents ).toHaveClass( "dropdown-activated" );
 
-    dropdown.removePanels();
+    dropdown.removePanels( dropdown.contents );
 
     expect( dropdown.contents ).not.toHaveClass( "dropdown-activated" );
+  } );
+
+  it( "Click and active the #1 dropdown, and remove others", () => {
+    dropdown.targets[ 0 ].click();
+    expect( dropdown.contents[ 0 ] ).toHaveClass( "dropdown-activated" );
+    expect( dropdown.contents[ 1 ] ).not.toHaveClass( "dropdown-activated" );
+  } );
+
+  it( "Click and active the #2 dropdown, and remove others", () => {
+    dropdown.targets[ 1 ].click();
+    expect( dropdown.contents[ 1 ] ).toHaveClass( "dropdown-activated" );
+    expect( dropdown.contents[ 0 ] ).not.toHaveClass( "dropdown-activated" );
   } );
 } );
