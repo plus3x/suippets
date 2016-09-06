1b01dfe4ae730f564746bbf8edb1d569d623883b
c4co <carllos.nc@gmail.com>
2016-09-06 23:16:19 +0100

    Adding notification js

---
+++ app/js/components/notification.js
@@ -0,0 +1,45 @@
+import $ from "jquery-slim"
+
+// support ES5
+
+var notification = {
+  wrapper: $( "[notification-wrapper]" ),
+  delay: 5000,
+};
+
+notification.detach = function( wrapper ) {
+  wrapper
+    .detach()
+    .addClass( "notification-wrapper" )
+    .prependTo( "html" );
+}
+
+notification.removeByTime = function( notifier ) {
+  setTimeout( function() {
+    notifier.remove();
+  }, notification.delay );
+}
+
+notification.removeByClick = function( notifier ){
+  notifier.on( "click", function() {
+    notifier.remove();
+  });
+}
+
+notification.create = function( message, classes ) {
+  var notifier = $( "<div> </div>" );
+  notifier.addClass( "notification " + classes );
+  notifier.text( message );
+  notifier.appendTo( notification.wrapper );
+
+  notification.removeByTime( notifier );
+  notification.removeByClick( notifier );
+
+  return notifier;
+}
+
+notification.init = function() {
+  notification.detach( notification.wrapper );
+}
+
+export default notification;
