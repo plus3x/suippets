1e813c5812815763ef40577e69eae54d3e028973
c4co <carllos.nc@gmail.com>
2016-09-06 23:10:58 +0100

    Adding component template for generator

---
+++ generator/component.template.js
@@ -0,0 +1,26 @@
+const componentTemplate = ( name ) => `
+@@include("parts/header.html", {"local": "../"})
+
+  <main class="main">
+    <section class="sidebar">
+      @@include("parts/sidebar.html", {"local": "../"})
+    </section>
+
+    <section class="center">
+      <div class="container">
+        <h1 class="page-title">${name}</h1>
+
+        <div class="page-example">
+          <h4 class="page-section"> Example </h4>
+          @@include("components/${name}/example.html")
+        </div>
+
+        @@include("components/${name}/code.html", {"local": "../"})
+      </div>
+    </section>
+  </main>
+
+@@include("parts/footer.html", {"local": "../"})
+`;
+
+module.exports = componentTemplate;
