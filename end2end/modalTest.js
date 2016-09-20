71f39b7f0526cab8619c544dc4cbc7e5e75f5574
c4co <carllos.nc@gmail.com>
2016-09-20 21:00:24 +0100

    Adding e2e modal test

---
+++ end2end/modalTest.js
@@ -0,0 +1,25 @@
+const host = "https://c4co.github.io/suippets/";
+
+module.exports = {
+  "Test modal component" : client => {
+    client
+      .url( host )
+      .waitForElementVisible( "body", 1000 )
+      .assert.title( "Suippets" )
+      .click( "a[href*='/pages/modal.html']" )
+
+      .click( "button[modal-target='1']" )
+      .assert.elementPresent( "[modal-wrapper='1']")
+      .assert.containsText( ".modal-activated .modal-content h1", "Modal 1" )
+      .click( "[modal-wrapper='1'] button[modal-close]" )
+
+      .pause(300)
+
+      .click( "button[modal-target='2']" )
+      .assert.elementPresent( "[modal-wrapper='2']")
+      .assert.containsText( ".modal-activated .modal-content h1", "Modal 2" )
+      .click( "[modal-wrapper='2'] button[modal-close]" )
+
+      .end();
+  }
+};
