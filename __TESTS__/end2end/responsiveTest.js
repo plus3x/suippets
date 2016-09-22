70c9b5155c9dbd25f19d17713c44a180c45fc404
c4co <carllos.nc@gmail.com>
2016-09-22 02:02:10 +0100

    Adding responsive test

---
+++ __TESTS__/end2end/responsiveTest.js
@@ -0,0 +1,21 @@
+require( 'dotenv' ).config( { silent: true } );
+
+const sauce = require( '../sauce' );
+
+module.exports = {
+  "Test responsive design": client => {
+    client
+      .url( process.env.SAUCE_HOST )
+      .waitForElementVisible( "body", 1000 )
+      .resizeWindow( 1000, 1000 )
+      .assert.cssProperty( ".header", "display", "none" )
+      .resizeWindow( 320, 1000 )
+      .assert.cssProperty( ".header", "display", "block" )
+      .assert.elementPresent( ".open-menu" )
+      .click( ".open-menu" )
+      .assert.cssClassPresent( "section.sidebar", "toggle-sidebar" )
+      .click( ".open-menu" )
+      .assert.cssClassNotPresent( "section.sidebar", "toggle-sidebar" )
+      .pause( 1000 ).end()
+  }
+}
