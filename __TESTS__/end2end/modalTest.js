ab25580db72a91a359cef6c451e7425ac62a5ab6
c4co <carllos.nc@gmail.com>
2016-09-22 02:02:00 +0100

    Adding new test cases

--- __TESTS__/end2end/modalTest.js
+++ __TESTS__/end2end/modalTest.js
@@ -3,8 +3,9 @@ require( 'dotenv' ).config( { silent: true } );
 const sauce = require( '../sauce' );
 
 module.exports = {
-  "Modal: open and close with [modal-close]" : client => {
+  "Modal: Verify elements" : client => {
     client
+      .resizeWindow(1000, 1000)
       .url( process.env.SAUCE_HOST )
       .waitForElementVisible( "body", 1000 )
 
@@ -14,7 +15,7 @@ module.exports = {
       .click( "a[href*='/pages/modal.html']" )
 
       .click( "button[modal-target='1']" )
-      .assert.elementPresent( "[modal-wrapper='1']")
+      .assert.elementPresent( "[modal-wrapper='1']" )
       .assert.containsText( ".modal-activated .modal-content h1", "Modal 1" )
       .click( "[modal-wrapper='1'] button[modal-close]" )
 
@@ -24,8 +25,31 @@ module.exports = {
       .assert.elementPresent( "[modal-wrapper='2']")
       .assert.containsText( ".modal-activated .modal-content h1", "Modal 2" )
       .click( "[modal-wrapper='2'] button[modal-close]" )
+  },
 
-      .end();
+  "Modal: Open and close with [modal-close]" : client => {
+    client
+      .pause(300)
+
+      .click( "button[modal-target='1']" )
+      .assert.cssClassPresent( "[modal-wrapper='1']", "modal-activated" )
+      .click( ".modal-activated button[modal-close]" )
+      .assert.cssClassPresent( "[modal-wrapper='1']", "modal-hidden" )
+  },
+
+  "Modal: Close with escape-key" : client => {
+    client
+      .pause( 300 )
+
+      .click( "button[modal-target='1']" )
+      .assert.cssClassPresent( "[modal-wrapper='1']", "modal-activated" )
+
+      .pause( 300 )
+
+      .keys([ client.Keys.ESCAPE ])
+      .assert.cssClassPresent( "[modal-wrapper='1']", "modal-hidden" )
+
+      .pause( 1000 ).end()
   },
 
   tearDown: sauce
