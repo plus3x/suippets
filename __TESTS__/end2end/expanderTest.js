311b1800b3a1bc3ec0028b88bf3fea1e33e4d049
c4co <carllos.nc@gmail.com>
2016-09-22 20:55:22 +0100

    Adding expander e2e test

---
+++ __TESTS__/end2end/expanderTest.js
@@ -0,0 +1,29 @@
+require( 'dotenv' ).config( { silent: true } );
+
+const sauce = require( '../sauce' );
+
+module.exports = {
+  "Expander: open and close": client => {
+    client
+      .url( process.env.SAUCE_HOST )
+      .waitForElementVisible( "body", 1000 )
+      .resizeWindow( 1000, 1000 )
+      .assert.title( "Suippets" )
+
+      .click( "a[href*='/pages/expander.html']" )
+      .assert.containsText( ".page-title", "Expander" )
+      .assert.hidden( ".expander-content" )
+
+      .click( "[expander-title]" )
+      .assert.cssClassPresent(".expander-title", "expander-title-activated")
+      .assert.visible( ".expander-content" )
+
+      .click( "[expander-title]" )
+      .assert.cssClassNotPresent(".expander-title", "expander-title-activated")
+      .assert.hidden( ".expander-content" )
+
+      .pause( 1000 ).end();
+  },
+
+  tearDown: sauce
+};
