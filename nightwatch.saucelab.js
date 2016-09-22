b2155f0c375f82aa1e31dc7dd2cdc2495237c51b
c4co <carllos.nc@gmail.com>
2016-09-22 20:54:50 +0100

    Removing safari browser

--- nightwatch.saucelab.js
+++ nightwatch.saucelab.js
@@ -75,13 +75,5 @@ module.exports = {
         "version" : "47"
       }
     },
-
-    "safari" : {
-      "desiredCapabilities" : {
-        "browserName" : "safari",
-        "platform" : "OS X 10.11",
-        "version" : "9.0"
-      }
-    }
   }
 }
