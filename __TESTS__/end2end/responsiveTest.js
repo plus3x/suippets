4845b2e8ddfb905cbfa58fa69be5cb5f679b92a0
c4co <carllos.nc@gmail.com>
2016-10-05 06:01:59 +0100

    Refactoring responsive test cases

--- __TESTS__/end2end/responsiveTest.js
+++ __TESTS__/end2end/responsiveTest.js
@@ -3,24 +3,31 @@ require( 'dotenv' ).config( { silent: true } );
 const sauce = require( '../sauce' );
 
 module.exports = {
-  "Responsive design": client => {
+  "Responsive: initial state": client => {
     client
       .url( process.env.SAUCE_HOST )
       .waitForElementVisible( "body", 1000 )
-
       .resizeWindow( 1000, 1000 )
       .assert.cssProperty( ".header", "display", "none" )
+  },
 
+  "Responsive: resize window's with to 320px": client => {
+    client
       .resizeWindow( 320, 1000 )
       .assert.cssProperty( ".header", "display", "block" )
       .assert.elementPresent( ".open-menu" )
+  },
 
+  "Responsive: open menu": client => {
+    client
       .click( ".open-menu" )
       .assert.cssClassPresent( "section.sidebar", "toggle-sidebar" )
+  },
 
+  "Responsive: close menu": client => {
+    client
       .click( ".open-menu" )
       .assert.cssClassNotPresent( "section.sidebar", "toggle-sidebar" )
-
       .end()
   },
 
