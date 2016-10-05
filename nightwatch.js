ac0108b2db695c319f29a2156c460341f3bf12e5
c4co <carllos.nc@gmail.com>
2016-10-05 06:05:31 +0100

    Changing default browser to chrome

--- nightwatch.js
+++ nightwatch.js
@@ -29,7 +29,7 @@ module.exports = {
         "path" : ""
       },
       "desiredCapabilities": {
-        "browserName": "firefox",
+        "browserName": "chrome",
         "javascriptEnabled": true,
         "acceptSslCerts": true
       }
