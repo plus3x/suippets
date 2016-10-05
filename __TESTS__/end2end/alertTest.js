657818b1ff042ff3fa7451a828bc78a5fd5cf8f3
c4co <carllos.nc@gmail.com>
2016-10-05 05:30:52 +0100

    Adding alert e2e test

---
+++ __TESTS__/end2end/alertTest.js
@@ -0,0 +1,34 @@
+require( 'dotenv' ).config( { silent: true } );
+
+const sauce = require( '../sauce' );
+
+module.exports = {
+  "Alert: Alert page": client => {
+    client
+      .url( process.env.SAUCE_HOST )
+      .waitForElementVisible( "body", 1000 )
+      .resizeWindow( 1100, 1000 )
+      .assert.title( "Suippets" )
+      .pause( 300 )
+      .click( "a[href*='/pages/alert.html']" )
+      .pause( 300 )
+      .assert.containsText( ".page-title", "Alert" )
+  },
+
+  "Alert: close": client => {
+    client
+      .click( ".alert-danger [alert-close]" )
+      .click( ".alert-info [alert-close]" )
+      .click( ".alert-success [alert-close]" )
+      .click( ".alert-warnning [alert-close]" )
+      .pause( 500 )
+      .assert.hidden( ".alert-danger" )
+      .assert.hidden( ".alert-info" )
+      .assert.hidden( ".alert-success" )
+      .assert.hidden( ".alert-warnning" )
+      .pause( 300 )
+      .end()
+  },
+
+  tearDown: sauce
+};
