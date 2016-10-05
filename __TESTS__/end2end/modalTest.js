d90efe1d215c982f01bac760657aff17ae485eef
c4co <carllos.nc@gmail.com>
2016-10-05 05:39:40 +0100

    Refactoring modal test cases

--- __TESTS__/end2end/modalTest.js
+++ __TESTS__/end2end/modalTest.js
@@ -3,53 +3,50 @@ require( 'dotenv' ).config( { silent: true } );
 const sauce = require( '../sauce' );
 
 module.exports = {
-  "Modal: Verify elements" : client => {
+  "Modal: Modal page" : client => {
     client
-      .resizeWindow(1000, 1000)
+      .resizeWindow( 1000, 1000 )
       .url( process.env.SAUCE_HOST )
       .waitForElementVisible( "body", 1000 )
-
-      .pause(300)
-
+      .pause( 300 )
       .assert.title( "Suippets" )
       .click( "a[href*='/pages/modal.html']" )
+      .pause( 300 )
       .assert.containsText( ".page-title", "Modal" )
-
-      .click( "button[modal-target='1']" )
-      .assert.elementPresent( "[modal-wrapper='1']" )
-      .assert.containsText( ".modal-activated .modal-content h1", "Modal 1" )
-      .click( "[modal-wrapper='1'] button[modal-close]" )
-
-      .pause(300)
-
-      .click( "button[modal-target='2']" )
-      .assert.elementPresent( "[modal-wrapper='2']")
-      .assert.containsText( ".modal-activated .modal-content h1", "Modal 2" )
-      .click( "[modal-wrapper='2'] button[modal-close]" )
   },
 
-  "Modal: Open and close with [modal-close]" : client => {
+  "Modal: Initial state of wrappers": client => {
     client
-      .pause(300)
-
-      .click( "button[modal-target='1']" )
-      .assert.cssClassPresent( "[modal-wrapper='1']", "modal-activated" )
-      .click( ".modal-activated button[modal-close]" )
+      .assert.hidden( "[modal-wrapper='1']" )
       .assert.cssClassPresent( "[modal-wrapper='1']", "modal-hidden" )
+      .assert.hidden( "[modal-wrapper='2']" )
+      .assert.cssClassPresent( "[modal-wrapper='2']", "modal-hidden" )
+      .pause( 300 )
   },
 
-  "Modal: Close with escape-key" : client => {
+  "Modal: Open first modal": client => {
     client
-      .pause( 300 )
-
-      .click( "button[modal-target='1']" )
+      .click( "[modal-target='1']" )
+      .assert.visible( "[modal-wrapper='1']" )
       .assert.cssClassPresent( "[modal-wrapper='1']", "modal-activated" )
-
       .pause( 300 )
+  },
 
+  "Modal: Close first modal with [modal-close]": client => {
+    client
+      .click( ".modal-activated [modal-close]" )
+      .pause( 300 )
+      .assert.hidden( "[modal-wrapper='1']" )
+  },
+
+  "Modal: Close first modal with ESCAPE key": client => {
+    client
+      .click( "[modal-target='1']" )
+      .pause( 300 )
+      .assert.visible( "[modal-wrapper='1']" )
       .keys( [ client.Keys.ESCAPE ] )
-      .assert.cssClassPresent( "[modal-wrapper='1']", "modal-hidden" )
-
+      .pause( 300 )
+      .assert.hidden( "[modal-wrapper='1']" )
       .end()
   },
 
