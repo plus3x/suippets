736110ec9f4b9cb42661e82d15d6b0f74672127d
c4co <carllos.nc@gmail.com>
2016-10-05 18:28:40 +0100

    Renaming variables and angs

--- app/js/components/tabs.js
+++ app/js/components/tabs.js
@@ -7,39 +7,39 @@ var tabs = {
   activatedTitleClass: "tab-title-activated"
 };
 
-tabs.initialTab = function( firstTabTitle, firstTabContent ) {
-  firstTabTitle.addClass( tabs.activatedTitleClass );
-  firstTabContent.show();
+tabs.initialTab = function( firstTitle, firstContent ) {
+  firstTitle.addClass( tabs.activatedTitleClass );
+  firstContent.show();
 };
 
-tabs.changeTab = function( tabTitle, tabWrapper, tabContent ) {
-  tabTitle.on( "click", function() {
+tabs.changeTab = function( title, wrapper, content ) {
+  title.on( "click", function() {
     var tabIndex = $( this ).attr( "tab-title" );
-    var thisTabContent = tabWrapper.find( "[tab-content=" + tabIndex + "]" );
-    var thisTabTitle = tabWrapper.find( "[tab-title=" + tabIndex + "]" );
+    var thiscontent = wrapper.find( "[tab-content=" + tabIndex + "]" );
+    var thistitle = wrapper.find( "[tab-title=" + tabIndex + "]" );
 
-    tabTitle.removeClass( tabs.activatedTitleClass );
-    tabContent.hide();
+    title.removeClass( tabs.activatedTitleClass );
+    content.hide();
 
-    thisTabTitle.addClass( tabs.activatedTitleClass );
-    thisTabContent.show();
+    thistitle.addClass( tabs.activatedTitleClass );
+    thiscontent.show();
   });
 };
 
 tabs.init = function() {
   tabs.all.each( function( index, element ) {
-    var tabWrapper = $( element );
-    var tabTitle = $( element ).find( "[tab-title]" );
-    var tabContent = $( element ).find( "[tab-content]" );
-    var firstIndex = tabTitle.first().attr( "tab-title" );
-    var firstTabTitle = $( element ).find( "[tab-title]" ).first();
-    var firstTabContent = tabWrapper.find( "[tab-content=" + firstIndex + "]" );
+    var wrapper = $( element );
+    var title = $( element ).find( "[tab-title]" );
+    var content = $( element ).find( "[tab-content]" );
+    var firstIndex = title.first().attr( "tab-title" );
+    var firstTitle = $( element ).find( "[tab-title]" ).first();
+    var firstContent = wrapper.find( "[tab-content=" + firstIndex + "]" );
 
-    tabContent.hide();
+    content.hide();
 
-    tabs.initialTab( firstTabTitle, firstTabContent );
+    tabs.initialTab( firstTitle, firstContent );
 
-    tabs.changeTab( tabTitle, tabWrapper, tabContent );
+    tabs.changeTab( title, wrapper, content );
   });
 };
 
