5e5d6f628cda0422749ad6ebd69cfb2c381c5fab
c4co <carllos.nc@gmail.com>
2016-10-05 06:02:26 +0100

    Refactoring tabs test cases

--- __TESTS__/end2end/tabsTest.js
+++ __TESTS__/end2end/tabsTest.js
@@ -3,25 +3,23 @@ require( 'dotenv' ).config( { silent: true } );
 const sauce = require( '../sauce' );
 
 module.exports = {
-  "Tabs: Verify elements at initial state": client => {
+  "Tabs: Tabs page": client => {
     client
       .url( process.env.SAUCE_HOST )
       .waitForElementVisible( "body", 1000 )
       .resizeWindow( 1000, 1000 )
       .assert.title( "Suippets" )
-
       .pause( 300 )
-
       .click( "a[href*='/pages/tabs.html']" )
-
       .pause( 300 )
-
       .assert.containsText( ".page-title", "Tabs" )
+  },
 
+  "Tabs: Initial state": client => {
+    client
       .assert.cssClassPresent( "[tab-title='1']", "tab-title-activated" )
       .assert.cssClassNotPresent( "[tab-title='2']", "tab-title-activated" )
       .assert.cssClassNotPresent( "[tab-title='2']", "tab-title-activated" )
-
       .assert.visible( "[tab-content='1']" )
       .assert.hidden( "[tab-content='2']" )
       .assert.hidden( "[tab-content='3']" )
@@ -30,12 +28,10 @@ module.exports = {
   "Tabs: Active tab #2" : client => {
     client
       .pause( 300 )
-
       .click( "[tab-title='2']" )
       .assert.cssClassPresent( "[tab-title='2']", "tab-title-activated" )
       .assert.cssClassNotPresent( "[tab-title='1']", "tab-title-activated" )
       .assert.cssClassNotPresent( "[tab-title='3']", "tab-title-activated" )
-
       .assert.visible( "[tab-content='2']" )
       .assert.hidden( "[tab-content='1']" )
       .assert.hidden( "[tab-content='3']" )
@@ -44,12 +40,10 @@ module.exports = {
   "Tabs: Active tab #3" : client => {
     client
       .pause( 300 )
-
       .click( "[tab-title='3']" )
       .assert.cssClassPresent( "[tab-title='3']", "tab-title-activated" )
       .assert.cssClassNotPresent( "[tab-title='1']", "tab-title-activated" )
       .assert.cssClassNotPresent( "[tab-title='2']", "tab-title-activated" )
-
       .assert.visible( "[tab-content='3']" )
       .assert.hidden( "[tab-content='1']" )
       .assert.hidden( "[tab-content='2']" )
@@ -58,16 +52,13 @@ module.exports = {
   "Tabs: Active tab #1" : client => {
     client
       .pause( 300 )
-
       .click( "[tab-title='1']" )
       .assert.cssClassPresent( "[tab-title='1']", "tab-title-activated" )
       .assert.cssClassNotPresent( "[tab-title='2']", "tab-title-activated" )
       .assert.cssClassNotPresent( "[tab-title='3']", "tab-title-activated" )
-
       .assert.visible( "[tab-content='1']" )
       .assert.hidden( "[tab-content='2']" )
       .assert.hidden( "[tab-content='3']" )
-
       .end()
   },
 
